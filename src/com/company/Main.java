fad5bbaafe754aa8d3a0d57da37171ca4bdf4358
gabo <gabriel.piles.glez@gmail.com>
2018-04-19 17:39:04 +0200

    - All asterisks exercises done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,26 +4,102 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        print_asterisk();
-        print_n_asterisk(3);
-        print_n_asterisk_in_column(7);
+//        print_asterisk();
+//        print_n_asterisk(3);
+//        print_n_asterisk_in_column(7);
+//        print_triangle(3);
+//        print_centered_triangle(5);
+//        print_diamond(3);
+        print_diamond_name("Gabo", 3);
     }
 
     public static void print_asterisk(){
-        System.out.println("*");
+        System.out.print("*");
     }
 
-    public static void print_n_asterisk(int asterisk_number){
+    public static void print_n_asterisk(int number){
 
-        for (int i = 0; i < asterisk_number; i++) {
-            System.out.print("*");
+        for (int i = 0; i < number; i++) {
+            print_asterisk();
+        }
+    }
+
+    public static void print_n_spaces(int number){
+
+        for (int i = 0; i < number; i++) {
+            System.out.print(" ");
         }
     }
 
     public static void print_n_asterisk_in_column(int column_number){
 
         for (int i = 0; i < column_number; i++) {
-            System.out.println("*");
+            print_asterisk();
+            System.out.print("\n");
         }
+    }
+
+    public static void print_triangle(int heigh){
+
+        int number_asterisk = 1;
+
+        for (int i = 0; i < heigh; i++) {
+
+            print_n_asterisk(number_asterisk);
+            System.out.print("\n");
+            number_asterisk += 1;
+        }
+    }
+
+    public static void print_top_centered_triangle(int heigh){
+
+        int number_spaces_begining_row = heigh - 1;
+        int number_asterisk = 1;
+
+        for (int i = 0; i < heigh-1; i++) {
+
+            print_n_spaces(number_spaces_begining_row);
+            print_n_asterisk(number_asterisk);
+            System.out.print("\n");
+            number_asterisk += 2;
+            number_spaces_begining_row -= 1;
+        }
+    }
+
+    public static void print_centered_triangle(int heigh){
+
+        print_top_centered_triangle(heigh);
+        print_n_asterisk(heigh * 2 -1);
+        System.out.print("\n");
+    }
+
+    public static void print_inverted_triangle(int heigh){
+
+        int number_spaces_begining_row = 1;
+        int number_asterisk = heigh * 2 -1;
+
+        for (int i = 0; i < heigh; i++) {
+
+            print_n_spaces(number_spaces_begining_row);
+            print_n_asterisk(number_asterisk);
+            System.out.print("\n");
+            number_asterisk -= 2;
+            number_spaces_begining_row += 1;
+        }
+    }
+
+    public static void print_diamond(int heigh){
+
+        print_centered_triangle(heigh);
+        print_inverted_triangle(heigh-1);
+
+    }
+
+    public static void print_diamond_name(String name, int heigh){
+
+        print_top_centered_triangle(heigh);
+        System.out.println(name);
+        print_inverted_triangle(heigh-1);
+
     }
 }
