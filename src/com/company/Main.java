66774f11b3b7759adb6430dfa8ce40e8ee280e44
gabo <gabriel.piles.glez@gmail.com>
2018-04-19 16:04:43 +0200

    - First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String foo = "1234";
+        System.out.println("It works!");
+        System.out.println(foo.length());
+    }
+}
