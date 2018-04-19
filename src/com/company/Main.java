aff6ef2a9640fbf6279cce443abff715d4f5bfbb
gabo <gabriel.piles.glez@gmail.com>
2018-04-19 16:17:53 +0200

    - First three exercises of the training

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String foo = "1234";
-        System.out.println("It works!");
-        System.out.println(foo.length());
+        print_asterisk();
+        print_n_asterisk(3);
+        print_n_asterisk_in_column(7);
+    }
+
+    public static void print_asterisk(){
+        System.out.println("*");
+    }
+
+    public static void print_n_asterisk(int asterisk_number){
+
+        for (int i = 0; i < asterisk_number; i++) {
+            System.out.print("*");
+        }
+    }
+
+    public static void print_n_asterisk_in_column(int column_number){
+
+        for (int i = 0; i < column_number; i++) {
+            System.out.println("*");
+        }
     }
 }
